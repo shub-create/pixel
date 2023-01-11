ea23b0653908a8309202efa7aa7df40c181b6ac2
shubham mittal <shubhammittal175@gmail.com>
2023-01-11 15:12:38 +0530

    zeet changes

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const commentRoute = require('./routes/comment');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const hostname ='0.0.0.0';
+
 app.use(cors());
 
 app.use(express.json());
